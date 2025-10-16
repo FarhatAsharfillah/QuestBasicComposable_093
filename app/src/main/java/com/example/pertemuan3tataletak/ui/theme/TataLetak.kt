bfb41a227bc32fa92e206854e554e84e36d4c1b2
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-16 17:54:54 +0700

    Imports dan Deklarasi Package

---
+++ app/src/main/java/com/example/pertemuan3tataletak/ui/theme/TataLetak.kt
@@ -0,0 +1,27 @@
+package com.example.pertemuan3tataletak.ui.theme
+
+package com.example.mylayout
+[cite_start]import androidx.compose.foundation.Image [cite: 1]
+[cite_start]import androidx.compose.foundation.background [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.Arrangement [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.Box [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.Column [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.Row [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.Spacer [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.fillMaxHeight [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.fillMaxWidth [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.height [cite: 1]
+[cite_start]import androidx.compose.foundation.layout.padding [cite: 1]
+[cite_start]import androidx.compose.material3.Text [cite: 1]
+[cite_start]import androidx.compose.runtime.Composable [cite: 1]
+[cite_start]import androidx.compose.ui.Alignment [cite: 1]
+[cite_start]import androidx.compose.ui.Modifier [cite: 1]
+[cite_start]import androidx.compose.ui.graphics.Color [cite: 1]
+[cite_start]import androidx.compose.ui.layout.ContentScale [cite: 1]
+[cite_start]import androidx.compose.ui.res.painterResource [cite: 1]
+[cite_start]import androidx.compose.ui.text.font.FontFamily [cite: 1]
+[cite_start]import androidx.compose.ui.text.font.FontWeight [cite: 1]
+[cite_start]import androidx.compose.ui.unit.dp [cite: 1]
+[cite_start]import androidx.compose.ui.unit.sp [cite: 1]
+// Beberapa baris impor yang kurang jelas diabaikan
+
