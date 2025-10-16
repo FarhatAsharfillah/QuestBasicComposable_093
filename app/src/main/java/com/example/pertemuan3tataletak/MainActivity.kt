b28b5c3ed154eae2d9ed4a96e730fd89cfad785a
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-16 18:23:19 +0700

    MainActivityClass1

--- app/src/main/java/com/example/pertemuan3tataletak/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3tataletak/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.pertemuan3tataletak
 
+package com.example.mylayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
