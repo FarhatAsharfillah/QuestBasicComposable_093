ff1d6449a77abcbf75c45b330406e4e146281223
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-16 18:26:53 +0700

    MainActivityClass2

--- app/src/main/java/com/example/pertemuan3tataletak/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3tataletak/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.pertemuan3tataletak
-
 package com.example.mylayout
 
 import android.os.Bundle
@@ -8,19 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pertemuan3tataletak.ui.theme.Pertemuan3TataLetakTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
